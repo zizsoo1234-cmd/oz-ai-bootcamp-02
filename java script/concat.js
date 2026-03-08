b278c6e9e0ca456a3d890b34aebda44831cff36a
zizsoo1234-cmd <zizsoo1234@gmail.com>
2026-03-08 14:08:32 +0900

    Create concat.js

---
+++ java script/concat.js
@@ -0,0 +1,10 @@
+// concatenate : 문자열을 +로 연결할 수 있다
+
+let firstName = "Alex";
+let lastName = "Kim";
+
+console.log("Hello, " + firstName + " " + lastName);
+
+// Template literal(Python: F-string)
+let message = `내 이름은 ${firstName} ${lastName}입니다.`;
+console.log(message);
