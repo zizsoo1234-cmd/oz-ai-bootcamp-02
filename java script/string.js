6004bb6283c7ffaebf67dcb47e02a1246970a7ed
zizsoo1234-cmd <zizsoo1234@gmail.com>
2026-03-08 14:23:37 +0900

    Create string.js

---
+++ java script/string.js
@@ -0,0 +1,9 @@
+// string = 문자열, 쌍따옴표/작은 따옴표로 감싼 값
+let username = "alex";
+let message = 'Hello';
+
+console.log(username);
+console.log(message);
+
+console.log(typeof username);
+console.log(typeof message);
