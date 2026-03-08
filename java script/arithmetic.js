8a2e7ea7ca11136032df9a8fd17b13a141f435ff
zizsoo1234-cmd <zizsoo1234@gmail.com>
2026-03-08 14:07:15 +0900

    Create arithmetic.js

---
+++ java script/arithmetic.js
@@ -0,0 +1,16 @@
+// 산술 연산자: +, -, *, /
+
+let a = 10;
+let b = 4;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+
+// 몫 계산
+console.log(Math.floor(10 / 3));
+
+// 나머지 연산자
+// 10 = 4 *2 + 2
+console.log(a % b);
