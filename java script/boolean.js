5ab408d6df543c59b937f55e280f33ce73c59c15
zizsoo1234-cmd <zizsoo1234@gmail.com>
2026-03-08 14:07:58 +0900

    Create boolean.js

---
+++ java script/boolean.js
@@ -0,0 +1,10 @@
+// boolean = 참/거짓 표현
+
+let isAdult = true;
+let hasTicket = false;
+
+console.log(isAdult);
+console.log(hasTicket);
+
+console.log(typeof isAdult);
+console.log(typeof hasTicket);
